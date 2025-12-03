1dc6b5b85bcf6d579769557d8416cc6601069eb2
Bryan Ramos <nuketara96@gmail.com>
2025-12-03 13:59:31 -0500

    updated th econfig function

--- nvim/.config/nvim/lua/Tie/lazy/ato-close-tag.lua
+++ nvim/.config/nvim/lua/Tie/lazy/ato-close-tag.lua
@@ -1,7 +1,15 @@
 return{
-  {
-    "windwp/nvim-ts-autotag",
-    ft = { "html", "javascriptreact", "typescriptreact", "xml" },
-    config = true,
-  },
+  "windwp/nvim-ts-autotag",
+  dependencies = "nvim-treesitter/nvim-treesitter",
+  ft = { "html", "javascriptreact", "typescriptreact", "xml" },
+  config = function()
+    require("nvim-ts-autotag").setup({
+      opts = {
+        enable_close = true,
+        enable_rename = true,
+        enable_close_on_slash = true,
+      },
+    })
+  end,
 }
+
