26fc7f45bcfb432c99ee43e82f43dbbf0e701de0
Bryan Ramos <nuketara96@gmail.com>
2025-12-26 13:43:16 -0500

    Added Conform and updated LSP-config

--- nvim/.config/nvim/lua/Tie/lazy/conform.lua
+++ nvim/.config/nvim/lua/Tie/lazy/conform.lua
@@ -1,18 +1,18 @@
 -- Example conform.nvim setup
 return {
-  'stevearc/conform.nvim',
-  lazy = false,
-  opts = {
-    formatters_by_ft = {
-      lua = { 'stylua' }, -- Use the installed stylua for Lua files
-      python = { 'black' },
-      -- etc.
-    },
+	"stevearc/conform.nvim",
+	lazy = false,
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" }, -- Use the installed stylua for Lua files
+			python = { "black" },
+			-- etc.
+		},
 
-    format_on_save = {
-      lsp_fallback = true, 
-      async = true,
-      timeout_ms = 500,
-    },
-  },
+		format_after_save = {
+			lsp_fallback = true,
+			async = true,
+			timeout_ms = 500,
+		},
+	},
 }
--- nvim/.config/nvim/lua/Tie/lazy/lsp.lua
+++ nvim/.config/nvim/lua/Tie/lazy/lsp.lua
@@ -1,74 +1,73 @@
 local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...)
-    vim.api.nvim_buf_set_keymap(bufnr, ...)
-  end
-  local opts = { noremap = true, silent = true }
+	local function buf_set_keymap(...)
+		vim.api.nvim_buf_set_keymap(bufnr, ...)
+	end
+	local opts = { noremap = true, silent = true }
 
-  -- Mappings for LSP features
-  buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-  buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-  buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-  buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-  buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-  buf_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-  buf_set_keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-  buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+	-- Mappings for LSP features
+	buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+	buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+	buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+	buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+	buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+	buf_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+	buf_set_keymap("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+	buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
 
-  -- Formatting on save
-  if client.name == "eslint_d" or client.name == "jsonls" then
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      buffer = bufnr,
-      callback = function()
-        vim.lsp.buf.format()
-      end,
-    })
-  end
+	-- Formatting on save
+	if client.name == "eslint_d" or client.name == "jsonls" then
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			buffer = bufnr,
+			callback = function()
+				vim.lsp.buf.format()
+			end,
+		})
+	end
 
-  -- Diagnostic keymaps
-  buf_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
-  buf_set_keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
-  buf_set_keymap("n", "<leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-  buf_set_keymap("n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+	-- Diagnostic keymaps
+	buf_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
+	buf_set_keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+	buf_set_keymap("n", "<leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+	buf_set_keymap("n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
 end
 
 return {
-  {
-    "williamboman/mason.nvim",
-    cmd = "Mason",
-    build = ":MasonUpdate", -- Optional: automatically update servers
-    config = function()
-      require("mason").setup()
-    end,
-  },
+	{
+		"williamboman/mason.nvim",
+		cmd = "Mason",
+		build = ":MasonUpdate", -- Optional: automatically update servers
+		config = function()
+			require("mason").setup()
+		end,
+	},
 
-  -- Bridges mason and lspconfig
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "neovim/nvim-lspconfig",
-    },
-    config = function()
-      require("mason-lspconfig").setup({
-        -- A list of language servers to automatically install
-        ensure_installed = { "lua_ls", "jsonls", "cssls", "html", "tsserver", "pyright", "dockerls", "yamlls", "eslint"},
-      })
-    end,
-  },
+	-- Bridges mason and lspconfig
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"neovim/nvim-lspconfig",
+		},
+		config = function()
+			require("mason-lspconfig").setup({
+				-- A list of language servers to automatically install
+				ensure_installed = { "lua_ls", "jsonls", "cssls", "html", "pyright", "dockerls", "yamlls", "eslint" },
+			})
+		end,
+	},
 
-  -- The LSP configuration repository
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "williamboman/mason-lspconfig.nvim",
-    },
-    -- Setup each language server with the same on_attach function
-    config = function()
-      vim.lsp.config("lua_ls",{ on_attach = on_attach })
-      vim.lsp.config("jsonls",{ on_attach = on_attach })
-      vim.lsp.config("cssls",{ on_attach = on_attach })
-      vim.lsp.config("html",{ on_attach = on_attach })
-
-    end,
-  },
+	-- The LSP configuration repository
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason-lspconfig.nvim",
+		},
+		-- Setup each language server with the same on_attach function
+		config = function()
+			vim.lsp.config("lua_ls", { on_attach = on_attach })
+			vim.lsp.config("jsonls", { on_attach = on_attach })
+			vim.lsp.config("cssls", { on_attach = on_attach })
+			vim.lsp.config("html", { on_attach = on_attach })
+		end,
+	},
 }
